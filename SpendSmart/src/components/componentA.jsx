48ba7c0e8b238aefe109128fda1aabe93c1bdf9d
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-05-19 19:41:33 +0500

    new

--- SpendSmart/src/components/componentA.jsx
+++ SpendSmart/src/components/componentA.jsx
@@ -2,7 +2,7 @@ import { userProvider } from "../context/contex"
 export function ComponentA(){
     return (
     <userProvider value={"Component A"}>
-    <div>Component A</div>
+    <div>Component A is a parent component </div>
     </userProvider>
 )
 }
